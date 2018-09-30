33d5b8fef77dbaeb94f4a0bf785808afe1845d65
jmh <javier.molina@p1labs.mx>
2018-09-30 14:04:22 -0500

    Constraints are added

--- largest_product_in_a_series.py
+++ largest_product_in_a_series.py
@@ -2,6 +2,14 @@
 
 
 def find_the_largest_product(len_of_N, K, N):
+    if len_of_N != len(N):
+        raise ValueError(
+            'The number of digits must be iqual to {}'.format(len_of_N)
+        )
+
+    if K > len_of_N:
+        raise ValueError('N must be greater than or equal to K')
+
     largestProduct = 0
     for consec_digit in range(len(N) - K):
         product = 1
