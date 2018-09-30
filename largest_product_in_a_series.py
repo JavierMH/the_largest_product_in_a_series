f985336201d11d6dbb50a5783c8b98f2e24916e2
jmh <javier.molina@p1labs.mx>
2018-09-30 14:08:39 -0500

    It is refactorized in a class

--- largest_product_in_a_series.py
+++ largest_product_in_a_series.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python
 
 
-def find_the_largest_product(len_of_N, K, N):
-    if len_of_N != len(N):
-        raise ValueError(
-            'The number of digits must be iqual to {}'.format(len_of_N)
-        )
+class LargestProductInASeries():
+    def find_the_largest_product(self, len_of_N, K, N):
+        if len_of_N != len(N):
+            raise ValueError(
+                'The number of digits must be iqual to {}'.format(len_of_N)
+            )
 
-    if K > len_of_N:
-        raise ValueError('N must be greater than or equal to K')
+        if K > len_of_N:
+            raise ValueError('N must be greater than or equal to K')
 
-    largestProduct = 0
-    for consec_digit in range(len(N) - K):
-        product = 1
-        for digit_position in range(consec_digit, consec_digit + K):
-            product *= int(N[digit_position: digit_position + 1])
-        if product > largestProduct:
-            largestProduct = product
+        largestProduct = 0
+        for consec_digit in range(len(N) - K):
+            product = 1
+            for digit_position in range(consec_digit, consec_digit + K):
+                product *= int(N[digit_position: digit_position + 1])
+            if product > largestProduct:
+                largestProduct = product
 
-    print(largestProduct)
+        print(largestProduct)
+        return largestProduct
 
 
-find_the_largest_product(10, 5, '3675356291')
+if __name__ == '__main__':
+    result = LargestProductInASeries().find_the_largest_product(10, 5, '3675356291')
