1713bfbbd42be5e8bbb8f414a0ea79f88e5252b5
jmh <javier.molina@p1labs.mx>
2018-09-30 15:05:41 -0500

    Tests are added for the _obtain_T_by_input method

--- tests.py
+++ tests.py
@@ -22,6 +22,26 @@ class ContainersTestCase(unittest.TestCase):
             stacks = LargestProductInASeries().run()
         self.assertEqual(stacks, expected_stacks)
 
+    def test_obtain_T_by_input(self):
+        T_more_than_permited = 101
+        T_less_than_permited = 0
+        T_in_a_correct_range = 50
+
+        with self.assertRaises(ValueError):
+            LargestProductInASeries()._constraints_for_number_of_test_cases(
+                T_more_than_permited
+            )
+        with self.assertRaises(ValueError):
+            LargestProductInASeries()._constraints_for_number_of_test_cases(
+                T_less_than_permited
+            )
+        self.assertIsNone(
+            LargestProductInASeries()._constraints_for_number_of_test_cases(
+                T_in_a_correct_range
+            ),
+            None
+        )
+
 
 if __name__ == '__main__':
     unittest.main()
